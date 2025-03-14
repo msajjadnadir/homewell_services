cb1b9d1e2d9f6412859195aa00fb0daa8eb59fb5
msajjadnadir <m.sajjsdnadir@gmail.com>
2025-03-14 22:02:15 +0500

    added claims and responsiveness

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -16,25 +16,25 @@ export default function DashboardPage() {
 
   return (
     <div className="flex flex-col w-full">
-      <div className="flex flex-row w-full justify-between">
+      <div className="flex flex-col md:flex-row w-full justify-between">
         <div className="flex flex-col">
-          <span className="font-satoshi text-[24px] font-medium leading-[32.4px] tracking-[0.016em] text-primaryText-DEFAULT">
+          <span className="font-satoshi text-[20px] md:text-[24px] font-medium leading-[27px] md:leading-[32.4px] tracking-[0.016em] text-primaryText-DEFAULT">
             HOMEWELL SERVICES LLC
           </span>
-          <div className="flex flex-col gap-4 mt-2">
-            <span className="font-satoshi text-[16px] font-medium leading-[21.6px] tracking-[0.016em] text-gray-500">
+          <div className="flex flex-col gap-2 md:gap-4 mt-2">
+            <span className="font-satoshi text-[14px] md:text-[16px] font-medium leading-[18.9px] md:leading-[21.6px] tracking-[0.016em] text-gray-500">
               Call In: (007) 944-2588
             </span>
-            <span className="font-satoshi text-[16px] font-medium leading-[21.6px] tracking-[0.016em] text-gray-500">
+            <span className="font-satoshi text-[14px] md:text-[16px] font-medium leading-[18.9px] md:leading-[21.6px] tracking-[0.016em] text-gray-500">
               Agency ID: 2044
             </span>
-            <span className="font-satoshi text-[16px] font-medium leading-[21.6px] tracking-[0.016em] text-gray-500">
+            <span className="font-satoshi text-[14px] md:text-[16px] font-medium leading-[18.9px] md:leading-[21.6px] tracking-[0.016em] text-gray-500">
               Saturday, February 15, 2025
             </span>
           </div>
         </div>
         <button
-          className="px-[27px] py-[15px] font-medium text-[20px] bg-[#A37EE6] text-white rounded-lg h-fit shadow-[0px_6.55px_9.83px_-3.28px_#0A0D120D,0px_19.66px_26.22px_-6.55px_#0A0D121A]"
+          className="mt-4 md:mt-0 px-[20px] md:px-[27px] py-[10px] md:py-[15px] font-medium text-[16px] md:text-[20px] bg-[#A37EE6] text-white rounded-lg h-fit shadow-[0px_6.55px_9.83px_-3.28px_#0A0D120D,0px_19.66px_26.22px_-6.55px_#0A0D121A]"
           onClick={() => router.push(paths.dashboard.dashboardSetup)}
         >
           Manage Dashboard
